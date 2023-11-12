3a93d1ea890f42b4c630a1594bf97ad1db7db209
youssef26567 <92630969+youssef26567@users.noreply.github.com>
2023-11-12 22:02:56 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,18 @@
+// Create web server
 const http = require('http');
 
+// Define server hostname and port
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Create server
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world!');
+  res.end('Hello, World!\n');
 });
 
-server.listen(3000, () => {
-  console.log('Server running on port 3000');
+// Start server
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
